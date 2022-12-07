acbf0f7c91dca8fc5daae0b6ae9d61b7e8b35f4b
sevenhallz <sevenhallz@hotmail.com>
2022-12-06 20:51:26 -0500

    lab final

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,13 +9,31 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+  //Write your code hereconst username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -23,6 +41,16 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
+});
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const user = req.params.user;
+    if (user){
+        delete auth_users[user]
+    }
+    res.send(`Friend with the email  ${user} deleted.`);
+
 });
 
 module.exports.authenticated = regd_users;
